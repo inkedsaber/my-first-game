89eb7b113d8487c9de1d4e820cbdb3f5768e7234
inkedsaber <inkedsaber@gmail.com>
2018-12-27 02:40:14 +0200

    New: first commit

---
+++ Scripts/HealthManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthManager : MonoBehaviour {
+
+    public int MaxHealth;
+    public int CurrentHealth;
+
+	// Use this for initialization
+	void Start() {
+		CurrentHealth = MaxHealth;
+	}
+
+	// Update is called once per frame
+	void Update() {
+
+	}
+
+    public void HurtPlayer(int Damage) {
+        CurrentHealth -= Damage;
+    }
+
+    public void HealPlayer(int HealAmount) {
+        CurrentHealth += HealAmount;
+        if (CurrentHealth > MaxHealth) {
+            CurrentHealth = MaxHealth;
+        }
+    }
+}
---
+++ Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float MoveSpeed;
+    public float JumpForce;
+
+    public Rigidbody2D RigidBody;
+
+	// Use this for initialization
+	void Start() {
+        RigidBody = GetComponent<Rigidbody2D>();
+	}
+
+	// Update is called once per frame
+	void Update() {
+        RigidBody.velocity = new Vector3(Input.GetAxis("Horizontal") * MoveSpeed, RigidBody.velocity.y);
+
+        if (RigidBody.velocity.y == 0) {
+            if (Input.GetButtonDown("Jump")) {
+                RigidBody.velocity = new Vector3(RigidBody.velocity.x, JumpForce);
+            }
+        }
+    }
+}
